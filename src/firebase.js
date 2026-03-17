e777a1e7160fcd5c3f4e5a59e10cdc6b44cc66d6
sonagra123pratik-art <sonagra123pratik@gmail.com>
2026-03-17 22:13:56 +0530

    chore: use Vite env vars for Firebase config; add .env.example

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBkYm5_PJZTRGxdX9_l5WwAVmZW24Yr_IU",
-  authDomain: "fluxehub-da40f.firebaseapp.com",
-  projectId: "fluxehub-da40f",
-  storageBucket: "fluxehub-da40f.appspot.com",
-  messagingSenderId: "823308042011",
-  appId: "1:823308042011:web:1e535c0a316c64df0f1dbe",
-  measurementId: "G-DGER5HKJ6Z"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBkYm5_PJZTRGxdX9_l5WwAVmZW24Yr_IU",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "fluxehub-da40f.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "fluxehub-da40f",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "fluxehub-da40f.appspot.com",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "823308042011",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:823308042011:web:1e535c0a316c64df0f1dbe",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-DGER5HKJ6Z"
 };
 
 const app = initializeApp(firebaseConfig);
